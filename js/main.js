073150d8225e02aaaa6f5f7898b6a72a5bb15cd4
Jerry Zhong <zhong20000905@qq.com>
2023-09-01 21:56:50 +0800

    remove debug output

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ parsers = {}
 chrome.runtime.onMessage.addListener((message, messageSender, sendResponse) => {
     if (message.type !== 'process page')
         return false
-    console.log("reveive message")
     let parser = parsers[message.website]
     if(parser !== undefined)
         parser()
